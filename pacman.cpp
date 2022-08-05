53e699addd99128374fe9556615201449c1aaabf
Adel Asgari <adelasgari@users.noreply.github.com>
2022-08-05 17:52:46 +0430

    complete comments
    complete comments on the code

--- pacman.cpp
+++ pacman.cpp
@@ -12,11 +12,11 @@ using namespace sf;
 bool winCheck(int map[22][19])
 {
 
-    for (size_t i = 0; i < 22; i++)//تعداد سطرهای نقشه بازی 22 می باشد
+    for (size_t i = 0; i < 22; i++) //تعداد سطرهای نقشه بازی 22 می باشد
     {
-        for (size_t j = 0; j < 19; j++)//نعداد ستون های نقشه بازی 19 می باشد
+        for (size_t j = 0; j < 19; j++) //نعداد ستون های نقشه بازی 19 می باشد
         {
-            if (map[i][j] == 0)//در نقشه بازی شماره 0 به معنی خوراکی عادی است
+            if (map[i][j] == 0) //در نقشه بازی شماره 0 به معنی خوراکی عادی است
             {
                 return false;
             }
@@ -31,27 +31,27 @@ void setmap(int map[22][19])
     {
         for (size_t j = 0; j < 19; j++)
         {
-            map[i][j] = 0;
+            map[i][j] = 0; //ابتدا تمام خانه های ماتریس با خوراکی های عادی پر می شود
         }
     }
     for (size_t i = 0; i < 19; i++)
     {
-        map[0][i] = 2;
+        map[0][i] = 2; //سطر اول را باد یوار پر میکند.(دیوار بالا)
     }
     for (size_t i = 0; i < 22; i++)
     {
-        map[i][0] = 2;
+        map[i][0] = 2; //ستون اول را با دیوار پر میکنیم(دیوتر سمت چپ)
     }
     for (size_t i = 0; i < 19; i++)
     {
-        map[21][i] = 2;
+        map[21][i] = 2; //  (سطر آخر را با دیوار پر میکنیم(دیوار پایین
     }
     for (size_t i = 0; i < 22; i++)
     {
-        map[i][18] = 2;
+        map[i][18] = 2; //(ستون اخر را با دیوار پر میکنیم(ستون سمت راستا
     }
 
-    map[1][9] = 2;
+    map[1][9] = 2; // دیوار کشیده ایم
     map[2][2] = map[2][3] = map[2][5] = map[2][6] = map[2][7] = map[2][9] = map[2][11] = map[2][12] = map[2][13] = map[2][15] = map[2][16] = 2;
     map[3][2] = map[3][3] = map[3][5] = map[3][6] = map[3][7] = map[3][9] = map[3][11] = map[3][12] = map[3][13] = map[3][15] = map[3][16] = 2;
 
@@ -60,9 +60,9 @@ void setmap(int map[22][19])
     map[7][1] = map[7][2] = map[7][3] = map[7][5] = map[7][6] = map[7][7] = map[7][9] = map[7][11] = map[7][12] = map[7][13] = map[7][15] = map[7][16] = map[7][17] = 2;
     map[8][1] = map[8][2] = map[8][3] = map[8][5] = map[8][13] = map[8][15] = map[8][16] = map[8][17] = 2;
     map[9][1] = map[9][2] = map[9][3] = map[9][5] = map[9][7] = map[9][8] = map[9][10] = map[9][11] = map[9][13] = map[9][15] = map[9][16] = map[9][17] = 2;
-    // door of ghosts palce
+    // درب ختنه روح ها
     map[9][9] = 5;
-
+    //خانه های پوچ
     map[10][0] = map[10][1] = map[10][2] = map[10][3] = map[10][8] = map[10][9] = map[10][10] = map[10][15] = map[10][16] = map[10][17] = map[10][18] = 3;
     map[10][7] = map[10][11] = 2;
     map[11][1] = map[11][2] = map[11][3] = map[11][5] = map[11][7] = map[11][8] = map[11][9] = map[11][10] = map[11][11] = map[11][13] = map[11][15] = map[11][16] = map[11][17] = 2;
@@ -74,145 +74,148 @@ void setmap(int map[22][19])
     map[17][1] = map[17][3] = map[17][5] = map[17][7] = map[17][8] = map[17][9] = map[17][10] = map[17][11] = map[17][13] = map[17][15] = map[17][17] = 2;
     map[18][5] = map[18][9] = map[18][13] = 2;
     map[19][2] = map[19][3] = map[19][2] = map[19][4] = map[19][5] = map[19][6] = map[19][7] = map[19][9] = map[19][11] = map[19][12] = map[19][13] = map[19][14] = map[19][15] = map[19][16] = 2;
-    // pacman place
+    // موقعیت اولیه پک من
     map[12][9] = 3;
 
-    // power points
+    // خوراکی های قدرتی
     map[14][1] = 1;
     map[1][1] = 1;
     map[3][17] = 1;
     map[20][17] = 1;
 }
+// تابع برای ترسیم نقشه
 void drawmap(RenderWindow &b, int map[22][19])
 {
-    float x = 20;
-    float y = 200;
+    float x = 20;  // مختصات xنقشه
+    float y = 200; //مختصات y نقشه
     for (size_t i = 0; i < 22; i++)
     {
         for (size_t j = 0; j < 19; j++)
         {
             if (map[i][j] == 0)
             {
-                CircleShape c(2);
-                c.setFillColor(Color::Yellow);
-                c.setPosition(Vector2f(x, y));
+                CircleShape c(2);              //ترسیم دایره به شعاع 2
+                c.setFillColor(Color::Yellow); //تعیین رنگ دایره
+                c.setPosition(Vector2f(x, y)); //مختصات گوشه دایره
                 b.draw(c);
             }
             else if (map[i][j] == 1)
             {
-                CircleShape c(5);
+
+                CircleShape c(5); //تعیین اندازه دایره دیوار
                 c.setFillColor(Color::White);
                 c.setPosition(Vector2f(x, y));
                 b.draw(c);
             }
             else if (map[i][j] == 2)
             {
+                //کد های بخش ترسیم دیوار
                 // RectangleShape r(Vector2f(10, 10));
                 // r.setPosition(Vector2f(x, y));
                 // b.draw(r);
             }
 
-            x += 25;
+            x += 25; //فاصله دو ستون 25 است
         }
-        y += 24.9;
-        x = 20;
+        y += 24.9; //فاصله دو سطر 24.9 است
+        x = 20;    //مختصات ط اولین خانه 20 میباشد
     }
 }
 
 int main()
 {
-    srand(time(0));
-    RenderWindow MainWindow(VideoMode(500, 800), "Adel Asgari 40012358027");
-    // MainWindow.setFramerateLimit(60);
+    srand(time(0));                                                          //باعث ایجاد اعداد تصادفی میشود
+    RenderWindow MainWindow(VideoMode(500, 800), "Adel Asgari 40012358027"); //ابعاد پنجره 500 در 800 میباشد
+    // MainWindow.setFramerateLimit(60);//حد اکثر تعداد فریم 60 میباشد
     Image icon;
-    icon.loadFromFile("Images/pacpac.png");
+    icon.loadFromFile("Images/pacpac.png"); //با رگذاری ایکون پروژه
     MainWindow.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
 
     Texture logo;
-    logo.loadFromFile("Images/logo.png");
+    logo.loadFromFile("Images/logo.png"); //بارگذاری بنر بازی
     RectangleShape banner(Vector2f(500, 150));
     banner.setPosition(Vector2f(0, 0));
     banner.setTexture(&logo);
 
-    Vector2i andaze(33, 33);
+    Vector2i andaze(33, 33); //اندازه کاراکتر پک من
 
     Texture pacmanTexture;
-    pacmanTexture.loadFromFile("Images/pacman.png");
+    pacmanTexture.loadFromFile("Images/pacman.png"); //بارگذاری عکس پک من
 
     Sprite PacMan(pacmanTexture);
     PacMan.setTextureRect(IntRect(0, 0, andaze.x, andaze.y));
     PacMan.setScale(Vector2f(0.8, 0.8));
 
-    int CountOfLifes = 3;
+    int CountOfLifes = 3; //تعداد جانهای بارگذاری شده
 
-    float x = 245;
-    float y = 487;
+    float x = 245; //مختصات x پک من
+    float y = 487; //مختصات y پک من
 
     int normalizeX = 0;
     int normalizeY = 0;
 
-    Event event;
-    Time zamaneseparishode;
+    Event event;//رویداد های صفحه کلید و موس را دریافت میکند
+    Time zamaneseparishode;//زمان سپری شده
 
-    Clock clock;
+    Clock clock;//برای اندازه گیری زمان
 
-    float zamaneanimation = 0.2;
-    int tedadFrame = 3;
+    float zamaneanimation = 0.2;//مدت زمتن اجرای انیمیشن پک من
+    int tedadFrame = 3;//تعدتد عکس هتی تشکیل دهنده انیمیشن پک من
 
-    float sorat = 1;
+    float sorat = 1;//ضریب سرعت پک من
 
     Texture worldmap;
-    worldmap.loadFromFile("Images/map.png");
+    worldmap.loadFromFile("Images/map.png");//بارگذاری عکس نقشه با زی
     Sprite worldsprite;
     worldsprite.setTexture(worldmap);
     worldsprite.setPosition(Vector2f(20, 200));
-    worldsprite.setScale(Vector2f(1.025, 1.065));
+    worldsprite.setScale(Vector2f(1.025, 1.065));//تغییر مقیاس نقشه بازی
 
-    int map[22][19];
-    setmap(map);
+    int map[22][19];//تعیین نقشه بازی 
+    setmap(map);//پر کردن نقشه بازی
 
     Font font;
-    font.loadFromFile("Fonts/times.ttf");
+    font.loadFromFile("Fonts/times.ttf");//بارگذاری فونت بازی
 
-    Text matneEmtiaz;
+    Text matneEmtiaz;//متن امتیاز بازی
     matneEmtiaz.setString("Emtiaz : 0");
     matneEmtiaz.setFont(font);
     matneEmtiaz.setPosition(Vector2f(20, 160));
     // matneEmtiaz.scale(Vector2f(0.8, 0.8));
     matneEmtiaz.setFillColor(Color::Yellow);
-    matneEmtiaz.setCharacterSize(28);
+    matneEmtiaz.setCharacterSize(28);//اندازه متن
     matneEmtiaz.setOutlineColor(Color::Magenta);
     matneEmtiaz.setOutlineThickness(2);
 
-    int emtiaz = 20;
-    int emtiazeHarNoghte = 10;
-    int emtiazeHarNoghteBozorg = 50;
+    int emtiaz = 20;//امتیاز اولیه بازی
+    int emtiazeHarNoghte = 10;//امتیاز هر خوراک عادی
+    int emtiazeHarNoghteBozorg = 50;//امتیاز هر خوراک قدرتی
     int forsatebazi = 2;
 
-    int jahatePacMan = 3;
+    int jahatePacMan = 3;//جهت اولیه پک-من
 
     Texture scareGhostTexture;
-    scareGhostTexture.loadFromFile("Images/scaredghost.png");
+    scareGhostTexture.loadFromFile("Images/scaredghost.png");//بارگزاری روح ترسیده
     Texture redGhostTexture;
-    redGhostTexture.loadFromFile("Images/redghost.png");
+    redGhostTexture.loadFromFile("Images/redghost.png");//بارگزاری روح قرمز
     Ghost redGhost(245, 375, redGhostTexture, scareGhostTexture, 1, 1, 1);
 
-    int ghostStep = 1;
+    int ghostStep = 1;//مشخص کننده گام حرکت ارواح
 
     Texture cyanGhostTexture;
-    cyanGhostTexture.loadFromFile("Images/cyanghost.png");
+    cyanGhostTexture.loadFromFile("Images/cyanghost.png");//بارگزاری روح فیروزه ای
     Ghost cyanGhost(200, 435, cyanGhostTexture, scareGhostTexture, 1, 1, 1);
 
     Texture orangeGhostTexture;
-    orangeGhostTexture.loadFromFile("Images/orangeghost.png");
+    orangeGhostTexture.loadFromFile("Images/orangeghost.png");//بارگزاری روح نارنجی
     Ghost orangeGhost(228, 435, orangeGhostTexture, scareGhostTexture, 1, 1, 1);
 
     Texture pinkGhostTexture;
-    pinkGhostTexture.loadFromFile("Images/pinkghost.png");
+    pinkGhostTexture.loadFromFile("Images/pinkghost.png");//بارگزاری روح صورتی
     Ghost pinkGhost(235, 435, pinkGhostTexture, scareGhostTexture, 1, 1, 1);
 
     Texture food;
-    food.loadFromFile("Images/foods.png");
+    food.loadFromFile("Images/foods.png");//بارگزاری غذا
     Sprite foodsprite;
     foodsprite.setTexture(food);
 
@@ -288,7 +291,7 @@ int main()
         Time deltatime = clock.restart();
         zamaneseparishode += deltatime;
         float zamanbarhasbesanieh = zamaneseparishode.asSeconds();
-        
+
         int frame = static_cast<int>((zamanbarhasbesanieh / zamaneanimation) * tedadFrame) % tedadFrame;
         PacMan.setTextureRect(IntRect(frame * andaze.x, 0, andaze.x, andaze.y));
 
